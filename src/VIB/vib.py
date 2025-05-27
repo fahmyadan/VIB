a1abd25e06c6f8bf0ccdee13cc8976e74b7e6033
fahmyadan <fahmy.adan14@imperial.ac.uk>
2025-05-26 18:14:39 -0700

    add lightning stuff

--- src/VIB/vib.py
+++ src/VIB/vib.py
@@ -1,12 +1,40 @@
 import torch 
 from VAEs.models import VAE
+from pytorch_lightning import LightningModule
 from torchvision.models import inception_v3
+import torch.nn.functional as F
 
 
-class VIB(VAE):
-
-    def __init__(self, in_channels, latent_dim, vib_h=299, vib_w=299,out_dims=1000,  hidden_dims = None, **kwargs):
-        super().__init__(in_channels, latent_dim, hidden_dims, **kwargs)
-
+class VIB(LightningModule):
+    def __init__(self, model_params, opt_params, **kwargs):
+        super().__init__()
+        self.vae = VAE(**model_params)
         self.feat_extractor = inception_v3(pretrained=True)
-        print('check')
+        self.opt_params = opt_params
+
+    def training_step(self, batch, batch_idx):
+        # training_step defines the train loop.
+        x, y = batch
+        x = x.view(x.size(0), -1)
+        z = self.encoder(x)
+        x_hat = self.decoder(z)
+        loss = F.mse_loss(x_hat, x)
+        return loss
+
+    def validation_step(self, batch, idx):
+
+        x, y = batch
+
+        pass 
+
+    def configure_optimizers(self):
+        optimizer = torch.optim.Adam(self.parameters(), lr=self.opt_params.LR, weight_decay=self.opt_params.weight_decay)
+        return optimizer
+
+# class VIB(VAE):
+
+#     def __init__(self, in_channels, latent_dim, vib_h=299, vib_w=299,out_dims=1000,  hidden_dims = None, **kwargs):
+#         super().__init__(in_channels, latent_dim, hidden_dims, **kwargs)
+
+#         self.feat_extractor = inception_v3(pretrained=True)
+        
---
+++ src/lightning.py
@@ -0,0 +1,85 @@
+import os
+from typing import Optional
+from torch.utils.data import DataLoader, TensorDataset
+import torchvision.transforms as transforms
+import torchvision.datasets as datasets
+import pytorch_lightning as pl
+import torch
+
+class ImageNetDataModule(pl.LightningDataModule):
+    def __init__(
+        self,
+        data_path: str = None,
+        train_batch_size: int = 64,
+        val_batch_size: int = 64,
+        num_workers: int = 4,
+        image_size: int = 224,
+        debug = False, 
+        patch_size: int = None, 
+    ):
+        super().__init__()
+        self.data_dir = data_path
+        self.batch_size_train = train_batch_size
+        self.batch_size_val = val_batch_size
+        self.num_workers = num_workers
+        self.image_size = image_size
+        self.debug = debug
+
+    def setup(self, stage: Optional[str] = None):
+        normalize = transforms.Normalize(
+            mean=[0.485, 0.456, 0.406],  # Standard ImageNet mean/std
+            std=[0.229, 0.224, 0.225],
+        )
+
+        train_transform = transforms.Compose([
+            transforms.RandomResizedCrop(self.image_size),
+            transforms.RandomHorizontalFlip(),
+            transforms.ToTensor(),
+            normalize,
+        ])
+
+        val_transform = transforms.Compose([
+            transforms.Resize(256),
+            transforms.CenterCrop(self.image_size),
+            transforms.ToTensor(),
+            normalize,
+        ])
+        if not self.debug:
+            self.train_dataset = datasets.ImageFolder(
+                os.path.join(self.data_dir, "train"), transform=train_transform
+            )
+            self.val_dataset = datasets.ImageFolder(
+                os.path.join(self.data_dir, "val"), transform=val_transform
+            )
+        else:
+            num_classes = 10
+            num_train = int(1e4)
+            num_val = 500
+            train_imgs = torch.randn((num_train, 3, self.image_size, self.image_size))
+            val_images = torch.randn(num_val, 3, self.image_size, self.image_size)
+            train_labels = torch.randint(0, num_classes, (num_train,))
+            val_labels = torch.randint(0, num_classes, (num_val,))
+
+            self.train_dataset = TensorDataset(train_imgs, train_labels)
+            self.val_dataset = TensorDataset(val_images, val_labels)
+
+    def train_dataloader(self):
+        return DataLoader(
+            self.train_dataset,
+            batch_size=self.batch_size_train,
+            shuffle=True,
+            num_workers=self.num_workers,
+            pin_memory=True,
+        )
+
+    def val_dataloader(self):
+        return DataLoader(
+            self.val_dataset,
+            batch_size=self.batch_size_val,
+            shuffle=False,
+            num_workers=self.num_workers,
+            pin_memory=True,
+        )
+    
+
+
